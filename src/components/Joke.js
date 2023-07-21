94e2ff6a3df468d576f6810c879b935cc8cee2d8
Adam Ingram <kr0nai84@gmail.com>
2023-07-20 21:18:02 -0700

    implemented components

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,8 +1,32 @@
 import React from "react";
+import { useParams, useHistory } from "react-router-dom";
 
-const Joke = () => {
+const Joke = (props) => {
+    const { jokes } = props;
+    const { jokeID } = useParams();
+    const joke = jokes.find(joke => joke.id === parseInt(jokeID));
+    const history = useHistory()
+    if (!joke) {
+        history.push('/')
+    }
     return (<div className="App">
-        Joke
+        {joke ?
+            <div>
+                <h2>
+                    {joke.type}
+                </h2>
+                {joke.setup ?
+                    <p>
+                        joke.setup
+                    </p> : <p></p>
+                }
+                <h3>
+                    {joke.punchline}
+                </h3>
+            </div> : <h1>
+                Error Message Here
+            </h1>
+    }
     </div>);
 }
 
--- src/components/JokeList.js
+++ src/components/JokeList.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Joke from "./joke";
+import { Link, useRouteMatch } from 'react-router-dom'
 
-const JokeList = () => {
-    return(<div className="App">
-    Joke List
-    <Joke />
-    </div>);
+const JokeList = (joke, key) => {
+    const { url } = useRouteMatch();
+    return (
+        <div className="App" key={key}>
+            <Link to={`${url}/${joke.joke[0].joke.id}`}>
+                <Joke />
+            </Link>
+        </div>);
 }
 
 
